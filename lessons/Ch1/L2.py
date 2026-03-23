f8e5be41cac1e7f758401b4fd76bd6e52ead834d
vhiv94 <vhiv94@gmail.com>
2026-03-23 00:19:13 -0500

    Pass Lesson 2: Find Minimum

--- lessons/Ch1/L2.py
+++ lessons/Ch1/L2.py
@@ -2,6 +2,35 @@ from functools import reduce
 
 
 def find_minimum(nums: list[float]) -> float:
+    if nums == []:
+        return None
     return reduce(lambda min, elem: elem if elem < min else min, nums, float("inf"))
 
-find_minimum([5, 10, -20])
+
+run_cases = [
+    ([7, 4, 3, 100, 2343243, 343434, 1, 2, 32], 1),
+    ([12, 12, 12], 12),
+    ([10, 200, 3000, 5000, 4], 4),
+]
+
+submit_cases = run_cases + [
+    ([1], 1),
+    ([1, 2, 3, 4, 5], 1),
+    ([5, 4, 3, 2, 1], 1),
+    ([100, 200, 300, 400, 500], 100),
+    ([500, 400, 300, 200, 100], 100),
+    ([], None),
+]
+
+
+def test(input1, expected_output):
+    print("---------------------------------")
+    print(f"Inputs: {input1}")
+    result = find_minimum(input1)
+    print(f"Expected: {expected_output}")
+    print(f"Actual:   {result}")
+    if result == expected_output:
+        print("Pass")
+        return True
+    print("Fail")
+    return False
---
+++ main.py
@@ -0,0 +1,19 @@
+from lessons.Ch1.L2 import submit_cases, test
+
+def main():
+    passed = 0
+    failed = 0
+    for test_case in submit_cases:
+        correct = test(*test_case)
+        if correct:
+            passed += 1
+        else:
+            failed += 1
+    if failed == 0:
+        print("============= PASS ==============")
+    else:
+        print("============= FAIL ==============")
+    print(f"{passed} passed, {failed} failed")
+
+if __name__ == "__main__":
+    main()
