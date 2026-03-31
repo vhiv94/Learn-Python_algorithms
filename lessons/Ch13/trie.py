75ae7942f7599814bd22f6910c658e412005c64e
vhiv94 <vhiv94@gmail.com>
2026-03-31 09:42:40 -0500

    Guess Pass Lesson 2: Exists

--- lessons/Ch13/trie.py
+++ lessons/Ch13/trie.py
@@ -24,8 +24,18 @@ class Trie:
         next.setdefault(self.end_symbol, True)
         return self
     
+    def exists(self, word: str) -> bool:
+        word.lower()
+        next = self.root
+        for char in word:
+            if char not in next:
+                return False
+            next = next.get(char)
+        return next.get(self.end_symbol)
+    
     def __repr__(self) -> str:
         return json.dumps(trie.root, sort_keys=True, indent=2)
 
 trie = Trie(["hi", "hello", "help"])
+trie.exists("hello")
 print(trie)
